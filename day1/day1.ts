d8450b17f695287bbe6185a2ad0a766f41c9a177
Morgane Delmas <mdelmas93@gmail.com>
2023-12-01 19:17:16 +0100

    Day 1 : testing other solution for part 2

--- day1/day1.ts
+++ day1/day1.ts
@@ -25,6 +25,8 @@ const numberCorrespondances = new Map([
 const isNumber: (char: string) => boolean = (char) =>
   char >= "0" && char <= "9";
 
+// Part 2 first version : replacing every word in the line
+/*
 const processLinePart2: (line: string) => string = (line) => {
   let processedLine = "";
 
@@ -47,17 +49,64 @@ const processLinePart2: (line: string) => string = (line) => {
 
   return processedLine;
 };
+*/
+
+// Part 2 second version : looking for first and last match in the line
+// Quick quick, just to test the method !
+const processLinePart2V2: (line: string) => number = (line) => {
+  let firstNumber;
+  let lastNumber;
+
+  // start of string
+  for (let i = 0; i < line.length; i++) {
+    // char is a number
+    if (firstNumber === undefined && isNumber(line[i])) {
+      firstNumber = line[i];
+      break;
+    }
+
+    // char is not a number => check for word correspondance
+    let matched = false;
+    for (let [word, number] of numberCorrespondances) {
+      if (line.substring(i).startsWith(word)) {
+        firstNumber = number;
+        matched = true;
+        break;
+      }
+    }
+    if (matched) break;
+  }
+
+  // end of string
+  for (let i = line.length - 1; i >= 0; i--) {
+    // char is a number
+    if (isNumber(line[i])) {
+      lastNumber = line[i];
+      break;
+    }
+
+    // char is not a number => check for word correspondance
+    let matched = false;
+    for (let [word, number] of numberCorrespondances) {
+      if (line.substring(i).startsWith(word)) {
+        lastNumber = number;
+        matched = true;
+        break;
+      }
+    }
+    if (matched) {
+      break;
+    }
+  }
+
+  return +`${firstNumber}${lastNumber}`;
+};
 
 const calibrationValuesPart1 = input.map((line) => {
   const lineNumbers = processLinePart1(line);
   return +`${lineNumbers[0]}${lineNumbers[lineNumbers.length - 1]}`;
 });
-
 console.log(`Day 1 part 1 answer : ${sum(calibrationValuesPart1)}`);
 
-const calibrationValuesPart2 = input.map((line) => {
-  const lineNumbers = processLinePart2(line);
-  return +`${lineNumbers[0]}${lineNumbers[lineNumbers.length - 1]}`;
-});
-
+const calibrationValuesPart2 = input.map(processLinePart2V2);
 console.log(`Day 1 part 2 answer : ${sum(calibrationValuesPart2)}`);
