9d87b0d8b78800f91e6099f931845a41e25f7c6e
Morgane Delmas <mdelmas93@gmail.com>
2023-12-04 12:59:31 +0100

    Day 4 : part 2

--- day4/day4.ts
+++ day4/day4.ts
@@ -5,6 +5,7 @@ interface Card {
   winningNumbers: number[];
   presentNumbers: number[];
   points: number;
+  copies: number;
 }
 let cards: Card[] = [];
 
@@ -28,16 +29,31 @@ fs.readFileSync("./input", "utf-8")
       winningNumbers,
       presentNumbers,
       points: 0,
+      copies: 1,
     });
   });
 
+// PROCESSING
 cards.forEach((card) => {
+  let numberOfWinningCards = 0; // count winning cards for PART 2
+
   card.presentNumbers.forEach((number) => {
     if (card.winningNumbers.includes(number)) {
-      card.points = card.points > 0 ? card.points * 2 : 1;
+      card.points = card.points > 0 ? card.points * 2 : 1; // count points for PART 1
+      numberOfWinningCards++;
     }
   });
+
+  // count copies for PART 2
+  for (let i = 0; i < numberOfWinningCards; i++) {
+    cards[card.number + i].copies += card.copies;
+  }
 });
 
+// PART 1
 let sumOfPoints = cards.reduce((sum, card) => sum + card.points, 0);
 console.log(`Day 4 part 1 : sum of points = ${sumOfPoints}`);
+
+// PART 2
+let sumOfCardsCopies = cards.reduce((sum, card) => sum + card.copies, 0);
+console.log(`Day 4 part 2 : sum of cards copies = ${sumOfCardsCopies}`);
