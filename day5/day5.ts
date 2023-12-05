6e6bc4a5b819d435ef0530132791e2fab161dff7
Morgane Delmas <mdelmas93@gmail.com>
2023-12-05 14:28:03 +0100

    Day 5 : part 2 finally working !!

--- day5/day5.ts
+++ day5/day5.ts
@@ -17,6 +17,18 @@ let seeds = input
   .map((seed) => +seed);
 let modifiedSeeds = seeds.map((seed) => seed); // copy of seeds to modify a seed only once (only basing myself on the initial value of seed)
 
+let seedsRanges: Range[] = [];
+seeds.forEach((_, index) => {
+  if (index % 2 !== 0) return;
+
+  const [startRange, rangeLength] = seeds.slice(index);
+  seedsRanges.push({
+    start: startRange,
+    end: startRange + rangeLength - 1,
+  });
+});
+let modifiedSeedsRanges: Range[] = []; // copy of seeds to modify a seed only once (only basing myself on the initial value of seed)
+
 const steps = [
   "seed-to-soil",
   "soil-to-fertilizer",
@@ -28,6 +40,85 @@ const steps = [
 ];
 let currentStep = -1;
 
+for (const line of input) {
+  if (line.startsWith(steps[currentStep + 1])) {
+    currentStep++;
+
+    seedsRanges = modifiedSeedsRanges.concat(
+      seedsRanges.filter((range) => range.start !== -1)
+    ); // save modified seed
+    modifiedSeedsRanges = [];
+
+    continue;
+  }
+
+  // else process the mapping
+  // line: destination range start, source range start, range length
+  const [startDestinationRange, startSourceRange, rangeLength] = line
+    .split(/\s+/)
+    .map((n) => +n);
+
+  const transformation = startDestinationRange - startSourceRange;
+  const sourceRange = {
+    start: startSourceRange,
+    end: startSourceRange + rangeLength - 1,
+  };
+
+  seedsRanges.forEach((seedsRange, index) => {
+    if (seedsRanges[index].start === -1) return;
+
+    if (
+      seedsRange.start >= sourceRange.start &&
+      seedsRange.end <= sourceRange.end
+    ) {
+      modifiedSeedsRanges.push({
+        start: seedsRange.start + transformation,
+        end: seedsRange.end + transformation,
+      });
+      seedsRanges[index] = {
+        start: -1,
+        end: -1,
+      };
+    } else if (
+      seedsRange.start >= sourceRange.start &&
+      seedsRange.start <= sourceRange.end &&
+      seedsRange.end >= sourceRange.end
+    ) {
+      modifiedSeedsRanges.push({
+        start: seedsRange.start + transformation,
+        end: sourceRange.end + transformation,
+      });
+      seedsRanges[index] = {
+        start: sourceRange.end + 1,
+        end: seedsRange.end,
+      };
+    } else if (
+      seedsRange.start < sourceRange.start &&
+      seedsRange.end >= sourceRange.start &&
+      seedsRange.end <= sourceRange.end
+    ) {
+      seedsRanges[index] = {
+        start: seedsRange.start,
+        end: sourceRange.start - 1,
+      };
+      modifiedSeedsRanges.push({
+        start: sourceRange.start + transformation,
+        end: seedsRange.end + transformation,
+      });
+    }
+  });
+}
+
+seedsRanges = modifiedSeedsRanges.concat(
+  seedsRanges.filter((range) => range.start !== -1)
+); // save modified seed
+console.log(
+  `Day 5 part 2 : new min location = ${Math.min(
+    ...seedsRanges.map((range) => range.start)
+  )}`
+);
+
+/*
 for (const line of input) {
   if (line.startsWith(steps[currentStep + 1])) {
     currentStep++;
@@ -53,93 +144,6 @@ for (const line of input) {
 
 seeds = modifiedSeeds; // save modified seed
 console.log(`Day 5 part 1 : min location = ${Math.min(...seeds)}`);
-
-// input.shift();
-/*
-// let seedsRanges: Range[] = [];
-// seeds.forEach((_, index) => {
-//   if (index % 2 !== 0) return;
-
-//   const [startRange, rangeLength] = seeds.slice(index);
-//   seedsRanges.push({ start: startRange, end: startRange + rangeLength - 1 });
-// });
-// let modifiedSeedsRanges = seedsRanges.map((seed) => ({ ...seed })); // copy of seeds to modify a seed only once (only basing myself on the initial value of seed)
-// console.log("starting ranges", seedsRanges);
-
-
-for (const line of input) {
-  if (line.startsWith(steps[currentStep + 1])) {
-    currentStep++;
-
-    seedsRanges = modifiedSeedsRanges; // save modified seed
-    modifiedSeedsRanges = seedsRanges.map((seed) => ({ ...seed }));
-
-    console.log("new step", steps[currentStep], "\tranges", seedsRanges);
-
-    continue;
-  }
-
-  // else process the mapping
-  // line: destination range start, source range start, range length
-  const [startDestinationRange, startSourceRange, rangeLength] = line
-    .split(/\s+/)
-    .map((n) => +n);
-  console.log(startDestinationRange, startSourceRange, rangeLength);
-
-  const transformation = startDestinationRange - startSourceRange;
-  const sourceRange = {
-    start: startSourceRange,
-    end: startSourceRange + rangeLength - 1,
-  };
-
-  seedsRanges.forEach((seedsRange, index) => {
-    if (
-      seedsRange.start >= sourceRange.start &&
-      seedsRange.end <= sourceRange.end
-    ) {
-      console.log("modifying range 1");
-      modifiedSeedsRanges[index] = {
-        start: seedsRange.start + transformation,
-        end: seedsRange.end + transformation,
-      };
-    } else if (
-      seedsRange.start >= sourceRange.start &&
-      seedsRange.start <= sourceRange.end &&
-      seedsRange.end >= sourceRange.end
-    ) {
-      console.log("modifying range 2");
-
-      modifiedSeedsRanges[index] = {
-        start: seedsRange.start + transformation,
-        end: sourceRange.end + transformation,
-      };
-      seedsRanges.push({
-        start: sourceRange.end + 1,
-        end: seedsRange.end,
-      });
-    } else if (
-      seedsRange.start < sourceRange.start &&
-      seedsRange.end >= sourceRange.start &&
-      seedsRange.end <= sourceRange.end
-    ) {
-      console.log("modifying range 3");
-      modifiedSeedsRanges[index] = {
-        start: seedsRange.start,
-        end: sourceRange.start - 1,
-      };
-      modifiedSeedsRanges.push({
-        start: sourceRange.start + transformation,
-        end: sourceRange.end + transformation,
-      });
-    }
-  });
-}
-
-seedsRanges = modifiedSeedsRanges; // save modified seed
-console.log(seedsRanges);
-console.log(
-  `Day 5 part 2 : new min location = ${Math.min(
-    ...seedsRanges.map((range) => range.start)
-  )}`
-);
+rangeSeeds = modifiedRangeSeeds; // save modified seed
+console.log(`Day 5 part 2 : new min location = ${Math.min(...rangeSeeds)}`);
 */
