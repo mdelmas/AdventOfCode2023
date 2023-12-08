28802dde339bfa0f962b2a0671c437abdcb2dc89
Morgane Delmas <mdelmas93@gmail.com>
2023-12-08 15:04:29 +0100

    Day 8 part 2 done (not bruteforcing this one ...)

--- day8/day8.ts
+++ day8/day8.ts
@@ -17,26 +17,71 @@ const directions: Map<string, number> = new Map([
 const sequence: string[] = input.shift()!.split("");
 input.shift();
 
+let currentNodes: string[] = [];
 const network: Map<string, [string, string]> = new Map();
 input.forEach((line) => {
   const [startingNode, nextNodes] = line.split(" = ");
 
+  if (startingNode.endsWith("A")) {
+    currentNodes.push(startingNode);
+  }
+
   const [_, leftNode, rightNode] = [
-    ...nextNodes.matchAll(/\(([A-Z]{3}), ([A-Z]{3})\)/g),
+    ...nextNodes.matchAll(/\(([A-Z0-9]{3}), ([A-Z0-9]{3})\)/g),
   ][0];
 
   network.set(startingNode, [leftNode, rightNode]);
 });
 
-// PART 1
 let totalSteps = 0;
-
-let currentStep = "AAA";
-while (currentStep !== "ZZZ") {
+// PART 1
+let currentNode = "AAA";
+if (!network.get(currentNode)) {
+  throw new Error(`Starting node doesn't exist`);
+}
+while (currentNode !== "ZZZ") {
   sequence.forEach((step) => {
     totalSteps++;
-    currentStep = network.get(currentStep)![directions.get(step)!];
+    currentNode = network.get(currentNode)![directions.get(step)!];
+  });
+}
+console.log(`Day 8 part 1 : total steps = ${totalSteps}`);
+
+// PART 2
+if (currentNodes.length === 0) {
+  throw new Error(`No starting node found`);
+}
+
+let steps: number[] = new Array(currentNodes.length).fill(0);
+while (
+  currentNodes.filter((currentNode) => currentNode.endsWith("Z")).length !==
+  currentNodes.length
+) {
+  sequence.forEach((step) => {
+    currentNodes
+      .map((currentNode, index) => ({ currentNode, index }))
+      .filter(({ currentNode }) => !currentNode.endsWith("Z"))
+      .forEach(({ currentNode, index }) => {
+        steps[index]++;
+        currentNodes[index] = network.get(currentNode)![directions.get(step)!];
+      });
   });
 }
 
-console.log(`Day 8 part 1 : total steps = ${totalSteps}`);
+const greatestCommonDivisor: (a: number, b: number) => number = (
+  a: number,
+  b: number
+) => {
+  let [biggest, smallest] = a > b ? [a, b] : [b, a];
+
+  if (smallest === 0) return biggest;
+  return greatestCommonDivisor(smallest, biggest % smallest);
+};
+const leastCommonMultiple = (a: number, b: number) =>
+  (a * b) / greatestCommonDivisor(a, b);
+
+console.log(
+  `Day 8 part 2 : total steps = ${steps.reduce((totalSteps, currentStep) =>
+    leastCommonMultiple(totalSteps, currentStep)
+  )}`
+);
